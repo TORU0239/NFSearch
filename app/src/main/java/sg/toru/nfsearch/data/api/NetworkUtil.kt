8de92a72bb7f407494e53cb6b8c6bbc78195e5cf
Toru Choi <blueocean239@gmail.com>
2020-03-08 15:21:37 +0800

    add network module

--- app/src/main/java/sg/toru/nfsearch/data/api/NetworkUtil.kt
+++ app/src/main/java/sg/toru/nfsearch/data/api/NetworkUtil.kt
@@ -54,9 +54,9 @@ object NetworkUtil {
     const val BASEURL = "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/"
     const val IMAGESEARCHURL = "Search/ImageSearchAPI"
 
-    private val HEADER_KEY_HOST = "x-rapidapi-host"
-    private val HEADER_VALUE_HOST = "contextualwebsearch-websearch-v1.p.rapidapi.com"
+    const val HEADER_KEY_HOST = "x-rapidapi-host"
+    const val HEADER_VALUE_HOST = "contextualwebsearch-websearch-v1.p.rapidapi.com"
 
-    private val HEADER_KEY_APIKEY = "x-rapidapi-key"
-    private val HEADER_VALUE_APIKEY = "0d1d22588dmsh064436da0035f61p175155jsndd90b4894d9f"
+    const val HEADER_KEY_APIKEY = "x-rapidapi-key"
+    const val HEADER_VALUE_APIKEY = "0d1d22588dmsh064436da0035f61p175155jsndd90b4894d9f"
 }
---
+++ app/src/main/java/sg/toru/nfsearch/di/module/NetworkModule.kt
@@ -0,0 +1,64 @@
+package sg.toru.nfsearch.di.module
+
+import dagger.Module
+import dagger.Provides
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import sg.toru.nfsearch.data.api.NetworkUtil
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+class NetworkModule {
+
+    @Singleton
+    @Provides
+    fun providesOkHttp3(
+        httpLoggingInterceptor: HttpLoggingInterceptor,
+        headerInterceptor:Interceptor
+    ):OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .addInterceptor(headerInterceptor)
+            .connectTimeout(10000, TimeUnit.MILLISECONDS)
+            .readTimeout(10000, TimeUnit.MILLISECONDS)
+            .writeTimeout(10000, TimeUnit.MILLISECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesHttpLoggingInterceptor() = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    @Singleton
+    @Provides
+    fun providesHeaderInterceptor():Interceptor {
+        return object: Interceptor {
+            override fun intercept(chain: Interceptor.Chain): Response {
+                val original = chain.request()
+                val request = original.newBuilder()
+                    .addHeader(NetworkUtil.HEADER_KEY_HOST, NetworkUtil.HEADER_VALUE_HOST)
+                    .addHeader(NetworkUtil.HEADER_KEY_APIKEY, NetworkUtil.HEADER_VALUE_APIKEY)
+                    .method(original.method, original.body)
+                    .build()
+                return chain.proceed(request)
+            }
+        }
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient:OkHttpClient):Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(NetworkUtil.BASEURL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
